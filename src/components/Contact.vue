017d538e3d2657257d977ea6f9cd343dd47bca67
Shawn Reuter <reutershawn45@gmail.com>
2026-08-28 11:56:11 -0500

    Update form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,1 +1,14 @@
-<script type="text/javascript" src="https://form.jotform.com/jsform/262394695130057"></script>
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const script = document.createElement('script')
+  script.type = 'text/javascript'
+  script.src = 'https://form.jotform.com/jsform/262394695130057'
+  document.body.appendChild(script)
+})
+</script>
+
+<template>
+  <div id="jotform-container"></div>
+</template>   
